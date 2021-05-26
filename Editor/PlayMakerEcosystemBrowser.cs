efc7274a048d09fd5506133badbf173a1901e752
Jean Fabre <contact@fabrejean.net>
2021-05-26 11:28:45 +0300

    testing simple editor window

---
+++ Editor/PlayMakerEcosystemBrowser.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace com.hutonggames.playmakereditor.addons.ecosystem
+{
+    public class PlayMakerEcosystemBrowser : EditorWindow
+	{
+        [MenuItem("PlayMaker/Addons/Ecosystem")]
+        private static void InitWindow()
+        {
+            Debug.Log("Hello");
+            PlayMakerEcosystemBrowser window = (PlayMakerEcosystemBrowser)EditorWindow.GetWindow(typeof(PlayMakerEcosystemBrowser));
+            window.titleContent = new GUIContent("Ecosystem");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Welcome", EditorStyles.boldLabel);
+
+        }
+    }
+}
