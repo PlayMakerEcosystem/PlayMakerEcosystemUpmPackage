fac3877a3b8486e69bdd891cbe6bee793bb0d0ab
Jean Fabre <contact@fabrejean.net>
2021-05-26 11:47:07 +0300

    bring back the full browser

---
+++ Editor/Browser/Classes/EcosystemEditorDefinesUtils.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine; // we need this within scripting define symbols code
+
+namespace com.hutonggames.playmakereditor.addons.ecosystem
+{
+	[InitializeOnLoad]
+	class EcosystemEditorDefinesUtils
+	{
+		static EcosystemEditorDefinesUtils ()
+		{
+			#if ! ECOSYSTEM 
+			Debug.Log("Setting Up Ecosystem Scripting define symbol 'ECOSYSTEM'"); 
+			Utils.MountScriptingDefineSymbolToAllTargets ("ECOSYSTEM");
+			#endif
+
+
+			#if ! ECOSYSTEM_0_6
+			Debug.Log("Setting Up Ecosystem Scripting define symbol 'ECOSYSTEM_0_6'"); 
+			Utils.MountScriptingDefineSymbolToAllTargets("ECOSYSTEM_0_6");
+			#endif
+
+			#if ! ECOSYSTEM_0_6_OR_NEWER
+			Debug.Log("Setting Up Ecosystem Scripting define symbol 'ECOSYSTEM_0_6_OR_NEWER'"); 
+			Utils.MountScriptingDefineSymbolToAllTargets("ECOSYSTEM_0_6_OR_NEWER");
+			#endif
+		}
+	}
+}
